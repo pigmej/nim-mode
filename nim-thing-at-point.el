99514c9da4c11a6beab6d156061b503197de0583
Jedrzej Nowak <pigmej@gmail.com>
2015-06-26 21:52:32 +0200

    working thing-at-point in minibuffer

--- nim-thing-at-point.el
+++ nim-thing-at-point.el
@@ -1,11 +1,9 @@
-;;; nim-sigs.el --- Call signatures and 'thing-at-point' thins for nim-mode
+;;; nim-thing-at-point.el --- 'thing-at-point' for nim-mode
 ;;; Code:
 
 (require 'cl-lib)
 (require 'nim-mode)
 
-
-;; call signature
 (defun nim-thing-at-point ()
   "Return call signature of current function for context at point."
   (interactive)
@@ -16,79 +14,24 @@
                       (nim-thing-at-point--format-minibuffer sigs))))))
 
 
-;; (defun nim-thing-at-point--propertize-params (params)
-;;   "Formats single params"
-;;   (mapcar
-;;    (lambda (x) (let* ((spl2 (split-string x ": "))
-;;                       (name (propertize (car spl2) 'face 'default))
-;;                       (type (propertize (cadr spl2) 'face 'font-lock-type-face)))
-;;                  (format "%s: %s" name type)))
-;;    (split-string params ", ")))
-
-;; (defun nim-thing-at-point--format-minibuffer-single (nim-type sig)
-;;   "Formats single sig details to use in minibuffer"
-;;   ;; TODO: apply nim-mode rules
-;;   (cond ((or (string-equal nim-type "skMethod")
-;;              (string-equal nim-type "skProc"))
-;;          (let* ((sig-string (nim-epc-forth sig))
-;;                 (params (substring sig-string (+ 1 (search "(" sig-string)) (search ")" sig-string))))
-;;            (format "(%s)"
-;;                    (mapconcat 'identity (nim-thing-at-point--propertize-params params) ", "))))
-;;         (t "")))
-
-
-;; (defun nim-thing-at-point--format-minibuffer-name (nim-type sig)
-;;   "Formats single sig name to use in minibuffer"
-;;   (format "%s: %s"
-;;    (substring nim-type 2)
-;;    (cond ((or (string-equal nim-type "skMethod")
-;;               (string-equal nim-type "skProc"))
-;;           (propertize (cadr (nim-epc-qualifiedPath sig)) 'face 'font-lock-function-name-face))
-;;          ((string-equal nim-type "skType")
-;;           (propertize (nim-epc-forth sig) 'face 'font-lock-type-face))
-;;          (t (nim-epc-forth sig)))))
-
-
-;; (let* ((sig (first sigs))
-;;        (nim-type (nim-epc-symkind sig))
-;;        (name (nim-thing-at-point--format-minibuffer-name nim-type sig))
-;;        (props (mapcar (lambda (x) (nim-thing-at-point--format-minibuffer-single nim-type x)) sigs)))
-;;   (message "%s %s" name (mapconcat 'identity props " | "))))
-
-
 (defun nim-thing-at-point--format-minibuffer (sigs)
   "Format callsignatures in minibuffer."
-  ;; (let* ((sig (first sigs))
-  ;;        (info (nim-thing-at-point-buffer (nim-epc-forth sig))))
-  ;;   ;; (message "%S" info)
-  ;;   (message "%s: %s" (nim-epc-symkind sig) info)))
-  (nim-thing-at-point-buffer sigs))
+  (let ((info (mapcar (lambda (x) (format "%s: %s" (nim-epc-symkind x)(nim-thing-at-point-buffer x))) sigs)))
+    (message (mapconcat 'identity info " | "))))
 
 
-(defun nim-thing-at-point-buffer (sigs)
+(defun nim-thing-at-point-buffer (sig)
   "Interts element to buffer to apply nim-mode color rules.
    then returns buffer-string"
-  ;;; TODO: create buffer first, apply modes first
-  ;;; FIXME: for some reason, contents are not propertized
-  (let* ((sig (first sigs))
-        (element (nim-epc-forth sig))
-        (buf (get-buffer-create "*nim-thing-at-point*")))
+  (let* ((element (nim-epc-forth sig))
+         (buf (get-buffer-create "*nim-thing-at-point*")))
     (with-current-buffer buf
-      ;; (view-mode 1)  ;; debug only
-      (nim-mode)
-      ;; (font-lock-unfontify-buffer)
       (erase-buffer)
       (insert element)
-      ;; (message (buffer-string))
-      (font-lock-fontify-buffer)
-      (deferred:$  ;;; hack
-        (deferred:wait 10) ; 1000msec
-        (deferred:nextc it
-          (lambda (x)
-            (with-current-buffer (get-buffer "*nim-thing-at-point*")
-              (message (buffer-string))))))
-      ;; )
-)))
+      (delay-mode-hooks (nim-mode))
+      (font-lock-default-function 'nim-mode)
+      (font-lock-default-fontify-buffer)
+      (buffer-string))))
 
 
 (defcustom nim-get-thing-at-point-delay 1
@@ -98,8 +41,8 @@
   "A variable where nim-mode keeps timer for signatures")
 
 (defun nim-enable-thing-at-point ()
+  "Starts timer for thing-at-point features "
   (interactive)
-  "Enable call signatures"
   (when nim-thing-at-point-timer
     (cancel-timer nim-thing-at-point-timer))
   (setq nim-thing-at-point-timer
